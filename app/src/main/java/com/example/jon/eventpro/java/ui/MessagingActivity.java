cedafcd999e94e33ed8c40098b40c4b772d389cf
j-hassel <jrh14j@my.fsu.edu>
2018-10-29 23:03:45 -0400

    changed searchbar in search fragment

--- app/src/main/java/com/example/jon/eventpro/java/ui/MessagingActivity.java
+++ app/src/main/java/com/example/jon/eventpro/java/ui/MessagingActivity.java
@@ -3,6 +3,7 @@ package com.example.jon.eventpro.java.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,6 +11,8 @@ import com.example.jon.eventpro.R;
 
 public class MessagingActivity extends AppCompatActivity
 {
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,6 +20,15 @@ public class MessagingActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messaging);
 
-
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                onBackPressed();
+            }
+        });
     }
 }
