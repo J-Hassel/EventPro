ff8447c2852345665e713592a499f9f9d880dc89
j-hassel <jrh14j@my.fsu.edu>
2018-11-02 16:20:11 -0400

    adding dependencies for messagind and databse

---
+++ app/src/main/java/com/example/jon/eventpro/java/MyFirebaseMessagingService.java
@@ -0,0 +1,17 @@
+package com.example.jon.eventpro.java;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class MyFirebaseMessagingService extends Service
+{
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return null;
+    }
+}
